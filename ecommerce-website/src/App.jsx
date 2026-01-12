6c9b1d3243a3540956d2e8cbda6530b9ceb97ae6
Abhinav9863 <abhinavvishnumohan21@gmail.com>
2026-01-12 15:55:16 +0530

    Moved all pages to react

--- ecommerce-website/src/App.jsx
+++ ecommerce-website/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import { HomePage } from './pages/HomePage.jsx'
 import { Routes, Route } from "react-router"
 import { CheckOut } from './pages/CheckOut.jsx'
+import  { Orders } from './pages/Orders.jsx'
+import { Tracking } from './pages/Tracking.jsx'
 
 function App() {
 
@@ -9,6 +11,8 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage />}> </Route>
       <Route path="/checkout" element={<CheckOut />}> </Route>
+      <Route path = "/orders" element = {<Orders />}> </Route>
+      <Route path = "/tracking" element = {<Tracking />}> </Route>
     </Routes>
 
 

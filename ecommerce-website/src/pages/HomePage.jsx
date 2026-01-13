0ce01629b5ddc35c1221be40a8d6731308730403
Abhinav9863 <abhinavvishnumohan21@gmail.com>
2026-01-13 10:32:55 +0530

    fetch  data from the backend.

--- ecommerce-website/src/pages/HomePage.jsx
+++ ecommerce-website/src/pages/HomePage.jsx
@@ -4,6 +4,15 @@ import "./HomePage.css";
 import { products } from "../../initial-code/data/products.js";
 
 export function HomePage() {
+    fetch('http://localhost:3000/api/products')
+        .then((response) => {
+            return response.json()
+            }).then((data) =>{
+                console.log(data);
+            
+        })
+
+
     return (
         <>
             <title>Ecommerce-website</title>

01350336ba6bf91e7d517d526db51ff5dad22b0d
Abhinav9863 <abhinavvishnumohan21@gmail.com>
2026-01-13 11:56:39 +0530

    Lifted cart upto App.jsx to share it

--- ecommerce-website/src/App.jsx
+++ ecommerce-website/src/App.jsx
@@ -4,13 +4,24 @@ import { Routes, Route } from "react-router"
 import { CheckOut } from './pages/CheckOut.jsx'
 import  { Orders } from './pages/Orders.jsx'
 import { Tracking } from './pages/Tracking.jsx'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+      const [cart , setCart] = useState([]);
+      useEffect(() => {
+          axios.get('http://localhost:3000/api/cart-items')
+            .then((response) => {
+                setCart(response.data)
+            });
+      },[]) 
+            
+
 
   return (
     <Routes>
-      <Route path="/" element={<HomePage />}> </Route>
-      <Route path="/checkout" element={<CheckOut />}> </Route>
+      <Route path="/" element={<HomePage cart = {cart}/>}> </Route>
+      <Route path="/checkout" element={<CheckOut cart = {cart}/>}> </Route>
       <Route path = "/orders" element = {<Orders />}> </Route>
       <Route path = "/tracking" element = {<Tracking />}> </Route>
     </Routes>
